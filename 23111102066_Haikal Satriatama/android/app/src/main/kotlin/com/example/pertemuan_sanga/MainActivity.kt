6bb88f4f942711e6334e64ddf92bc334bfa15efb
haikal123123 <2311102066@ittelkom-pwt.ac.id>
2026-06-18 20:19:01 +0700

    Delete 23111102066_Haikal Satriatama directory

--- 23111102066_Haikal Satriatama/android/app/src/main/kotlin/com/example/pertemuan_sanga/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pertemuan_sanga
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- 23111102066_Haikal Satriatama/android/app/src/main/kotlin/com/example/pertemuan_sedoso/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pertemuan_sedoso
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
